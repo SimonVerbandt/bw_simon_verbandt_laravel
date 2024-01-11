132a157a5d014d68ecdd744fc6afef357c7d6f36
Simon Verbandt <simon.verbandt@student.ehb.be>
2024-01-11 18:44:58 +0100

    admin middleware created

--- app/Http/Middleware/UserIsAdmin.php
+++ app/Http/Middleware/UserIsAdmin.php
@@ -6,18 +6,34 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class UserIsAdmin
 {
+
+    public function isAdmin(User $user): bool
+    {
+        $isAdmin = DB::table('users')->where('id', $user->id)->value('isAdmin', true);
+        return $isAdmin;
+    }
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
-    {   
+    {
+
         $user = Auth::user();
-        
+        if (!$user) {
+            return redirect()->route('login');
+        }
+
+        $admin = $this->isAdmin($user);
+        if (!$admin) {
+            return redirect()->route('dashboard');
+        }
         return $next($request);
     }
 }
