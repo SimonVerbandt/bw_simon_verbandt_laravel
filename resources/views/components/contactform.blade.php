2f868d02f2be6a97757ee5b2a2c1054e1b85162b
Simon Verbandt <simon.verbandt@student.ehb.be>
2024-01-12 22:22:03 +0100

    styling contact-form

--- resources/views/components/contactform.blade.php
+++ resources/views/components/contactform.blade.php
@@ -1,22 +1,62 @@
 <div class="contact-form">
     <form action="{{ route('contact-form.submit') }}" method="POST">
         @csrf
+        @method('POST')
         <div class="form-input">
             <label for="subject">Subject: </label>
-            <input type="text" name="subject" id="subject" class="form-control">
+            <x-text-input id="contact-form-subject" name="subject" type="text" class="mt-1 block w-full" autocomplete="current-password"/>
         </div>
         <div class="form-textarea">
             <label for="content">Content: </label>
-            <textarea type="text" name="content" id="content" class="form-control"></textarea>
+            <textarea id="contact-form-content" name="content" class="mt-1 block w-full" autocomplete="current-password"></textarea>
         </div>
-        <button type="submit" class="btn btn-primary">Send</button>
+        <div class="button">
+            <x-primary-button type="submit" class="btn btn-primary" >{{ __('Send') }}</x-primary-button>
+        </div>
     </form>
     <div class="errors">
         @error('subject')
-        <div class="alert alert-danger">{{ $message }}</div>
+            <div class="alert alert-danger">{{ $message }}</div>
         @enderror
         @error('content')
-        <div class="alert alert-danger">{{ $message }}</div>
+            <div class="alert alert-danger">{{ $message }}</div>
         @enderror
     </div>
 </div>
+
+<style>
+
+
+    #contact-form-content{
+        height: 200px;
+        border-radius: 1%;
+    }
+
+    .contact-form{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        }
+
+    .button{
+        margin-top: 10px;
+        display: flex;
+        justify-content: flex-end;
+        margin-right: 10px;
+    }
+
+    .errors{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top: 10px;
+    }
+
+    .alert{
+        margin-bottom: 10px;
+    }
+    .alert-danger{
+        color: red;
+    }
+</style>
