323e4e1af3382b8fd94827655228d0b09b8df694
Simon Verbandt <simon.verbandt@student.ehb.be>
2024-01-14 16:58:57 +0100

    faq show, create and store views

--- resources/views/admin/faq/create.blade.php
+++ resources/views/admin/faq/create.blade.php
@@ -1,7 +1,5 @@
 <x-app-admin>
-    <form method="POST" action="{{ route('faq.store') }}">
-            @csrf
-            @method('POST')
+<x-admin-form action="{{ route('faq.store') }}" enctype="{{null}}">
             <div class="form-input">
                 <label for="question">Question: </label>
                 <x-text-input id="faq-question" name="question" type="text" class="mt-1 block w-full"/>
@@ -18,48 +16,5 @@
                     @endforeach
                 </select>
             </div>
-            <x-primary-button type="submit" style="align-self:end;margin-top: 2rem; margin-right:1rem;">Post</x-primary-button>
-        </form>
-        <div class="errors" id="errorMessage">
-            @error('question')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-            @error('answer')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-            @error('category')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
-        <script>
-            setTimeout(() => {
-                document.getElementById('errorMessage').innerHTML = '';
-            }, 2000);
-        </script>
-        <style>
-            form{
-                display: flex;
-                flex-direction: column;
-            }
-            label, textarea, select, input{
-                padding: 1rem;
-            }
-            #errorMessage{
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-                margin-top: 10px;
-            }
-
-            .alert{
-                color: red;
-                margin-bottom: 1rem;
-            }
-            textarea{
-                height: 200px;
-                border-radius: 1%;
-            }
-        </style>
+</x-admin-form>
 </x-app-admin>
-
--- resources/views/admin/faq/index.blade.php
+++ resources/views/admin/faq/index.blade.php
@@ -8,7 +8,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <x-nav-link href="{{route('faq.create')}}" style="color: black; border: 1px solid black; border-radius: 5%; margin-bottom: 3rem; margin-left: 2rem">Create FAQ</x-dropdown-link>
+                    <x-nav-link href="{{route('faq.create')}}" style="color: black; border: 1px solid black; border-radius: 5%; margin-bottom: 3rem; margin-left: 2rem">Create FAQ</x-nav-link>
                     <ul>
                         @foreach ($faqs as $faq)
                             <div class="mb-4 ">
