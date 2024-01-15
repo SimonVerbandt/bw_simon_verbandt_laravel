80a87168e86fe9866bfef59d03e3129d4a14146e
Simon Verbandt <simon.verbandt@student.ehb.be>
2024-01-15 19:18:57 +0100

    finishing up, added resources and read me update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,13 +11,19 @@
 
     <h1>About</h1>
     <p>Here are all the sources used while developing this project:</p>
+    <br>
     <ul>
         <li><a href="https://tailwindcss.com/docs">Tailwind CSS</a></li>
         <li><a href="chat.openai.com">OpenAI ChatGPT</a></li>
         <li><a href="https://www.bunny.net/">Bunny CDN</a></li>
         <li><a href="https://favicon.io/favicon-converter/">Favicon.io</a></li>
         <li><a href="https://www.flaticon.com/free-icons/airplane">Flaticon</a></li>
-        <li><a href="https://docs.github.com/en/copilot/using-github-copilot/getting-started-with-github-copilot">GitHub CoPilot</a></li>
+        <li><a href="https://github.com/mwgg/Airports">Airport Dataset Github</a></li>
+        <li><a href="https://github.com/imgmongelli/airlines-logos-dataset">Airlines dataset Github</a></li>
+        <li><a href="https://cssgradient.io/">CSSGradient.io</a></li>
+        <li><a href="https://stackoverflow.com/">Stack Overflow</a></li>
+        <li><a href="https://gnews.io/">GNEWS API</a></li>
+
     </ul>
 </div>
 </div>
